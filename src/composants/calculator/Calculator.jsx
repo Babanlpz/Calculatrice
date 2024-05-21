2e180d3e9f472a9636557130373d8cd81bc83c51
Esteban <baab2an@live.fr>
2024-05-21 09:30:45 +0200

    FEAT title + background

--- src/composants/calculator/Calculator.jsx
+++ src/composants/calculator/Calculator.jsx
@@ -21,27 +21,33 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <div className="display">{display}</div>
-      <div className="buttons">
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("+")}>+</button>
-        <button onClick={() => handleClick("-")}>-</button>
-        <button onClick={() => handleClick("*")}>*</button>
-        <button onClick={() => handleClick("/")}>/</button>
-        <button onClick={calculateResult}>=</button>
-        <button onClick={clearDisplay}>c</button>
+    <>
+      <div>
+        <h1 className="title">Calculatrice</h1>
+
+        <div className="calculator">
+          <div className="display">{display}</div>
+          <div className="buttons">
+            <button onClick={() => handleClick("0")}>0</button>
+            <button onClick={() => handleClick("1")}>1</button>
+            <button onClick={() => handleClick("2")}>2</button>
+            <button onClick={() => handleClick("3")}>3</button>
+            <button onClick={() => handleClick("4")}>4</button>
+            <button onClick={() => handleClick("5")}>5</button>
+            <button onClick={() => handleClick("6")}>6</button>
+            <button onClick={() => handleClick("7")}>7</button>
+            <button onClick={() => handleClick("8")}>8</button>
+            <button onClick={() => handleClick("9")}>9</button>
+            <button onClick={() => handleClick("+")}>+</button>
+            <button onClick={() => handleClick("-")}>-</button>
+            <button onClick={() => handleClick("*")}>*</button>
+            <button onClick={() => handleClick("/")}>/</button>
+            <button onClick={calculateResult}>=</button>
+            <button onClick={clearDisplay}>c</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
